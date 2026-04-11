8e4bb6b8180c017b31825b6dbc62a86132389c98
eholmes83 <eholmes83@yahoo.com>
2026-04-10 22:18:33 -0400

    added dynamic data to user component and styling

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -3,8 +3,8 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-header',
     standalone: true, // don't need to set this for version 19+ but adding it for clarity true by default
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.css'
+    templateUrl: './header.html',
+    styleUrl: './header.css'
 
 })
 
