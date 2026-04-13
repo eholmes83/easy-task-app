6e9fbbe6718b1f8d4d7b79aada56e887e2b853ae
eholmes83 <eholmes83@yahoo.com>
2026-04-13 16:09:15 -0400

    adding custom event, property bindings, and multiple users as sample data for user cards. still WIP

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,19 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header";
 import { User } from "./user/user";
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [HeaderComponent, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('first-angular-app');
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log('Selected user ID:', id);
+  }
 }
